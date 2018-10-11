bb00029f047b795f5ff807545708963e733d1e21
mlfcjob@163.com <mlfcjob@163.com>
2018-10-11 15:28:16 +0800

    Fix crash when finish muxing.
    Fix crash when finish muxing.
    
    Signed-off-by: mlfcjob@163.com <mlfcjob@163.com>

--- FFmpeg_API_usage/src/8_1_Audio_Video_Muxer/close_media.c
+++ FFmpeg_API_usage/src/8_1_Audio_Video_Muxer/close_media.c
@@ -5,6 +5,6 @@ void close_stream(AVFormatContext *oc, OutputStream *ost) {
 	avcodec_close(ost->st->codec);
 	av_frame_free(&ost->frame);
 	av_frame_free(&ost->tmp_frame);
-	sws_freeContext(ost->swr_ctx);
+	sws_freeContext(ost->sws_ctx);
 	swr_free(&ost->swr_ctx);
 }
